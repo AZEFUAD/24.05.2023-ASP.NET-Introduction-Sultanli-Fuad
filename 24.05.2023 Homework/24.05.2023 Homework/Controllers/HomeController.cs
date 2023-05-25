19df547b115cf976de82259545cd6a3eaa5b4385
AZEFUAD <fuadkhs@code.edu.az>
2023-05-25 17:58:38 +0400

    Succesfully committed.

---
+++ 24.05.2023 Homework/24.05.2023 Homework/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace _24._05._2023_Homework.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Name = "Ali";
+            ViewBag.Surname = "Aliyev";
+            ViewBag.Age = 25;
+            ViewBag.Country = "Azərbaycan";
+            ViewBag.PhoneNumber = "+994551234567";
+            return View();
+        }
+    }
+}
